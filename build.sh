be5a6f4493e11e921b3aba2dc520912d0e8d924b
armandovloro-collab <armandovloro@asscat.edu.ph>
2026-05-06 10:05:42 +0800

    Move Vercel config to root directory

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Install dependencies from the correct directory
+pip install -r pixel_cost_optimizer/requirements.txt
+
+# Go into the project directory
+cd pixel_cost_optimizer
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate
--- pixel_cost_optimizer/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install dependencies
-pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Run migrations (Optional: Ensure database is up to date)
-python manage.py migrate
